45daac3a9afe6454fcb8fd34361e47e40b2b655a
Z3RØ_AN7RÄX <70908677+js13-Hacker@users.noreply.github.com>
2020-11-27 16:40:32 -0300

    Add files via upload

---
+++ CRYB4BY.sh
@@ -0,0 +1,197 @@
+#!/bin/bash
+COR_CINZA="\033[1;30m"
+COR_VERMELHO="\033[0;31m"
+COR_VERMELHO_CLARO="\033[1;31m"
+COR_AZUL_ESCURO="\033[0;34m"
+COR_AZUL_MARINHO="\033[1;34m"
+COR_AMARELO="\033[1;33m"
+COR_ROSA="\033[1;35m"
+COR_VERDE='\033[1;32m'
+clear
+printf $COR_VERDE
+   echo "Created by:Z3R0_AN7RAX"
+    printf $COR_VERDE
+     echo "GitHub:https://github.com/js13-Hacker"
+      printf $COR_AMARELO
+       echo "Gerador de nome hacker"
+        printf $COR_ROSA
+         echo "Escolha a inicial do seu nome:"
+printf $COR_VERDE
+echo "[1]A"
+echo "[2]B"
+echo "[3]C"
+echo "[4]D"
+echo "[5]E"
+echo "[6]F"
+echo "[7]G"
+echo "[8]H"
+echo "[9]I"
+echo "[10]J"
+echo "[11]K"
+echo "[12]L"
+echo "[13]M"
+echo "[14]N"
+echo "[15]O"
+echo "[16]P"
+echo "[17]Q"
+echo "[18]R"
+echo "[19]S"
+echo "[20]T"
+echo "[21]U"
+echo "[22]V"
+echo "[23]W"
+echo "[24]X"
+echo "[24]Y"
+echo "[25]Z"
+
+read nome
+if [ "$nome" = "1" ]
+then
+printf $COR_AZUL_MARINHO
+echo "Seu nome hacker é:SH4DOW"
+fi
+
+if [ "$nome" = "2" ]
+then
+printf $COR_AZUL_MARINHO
+echo "Seu nome hacker é:CHIEF"
+fi
+
+if [ "$nome" = "3" ]
+then
+printf $COR_AZUL_MARINHO
+echo "Seu nome hacker é:CYBER"
+fi
+
+if [ "$nome" = "4" ]
+then
+printf $COR_AZUL_MARINHO
+echo "Seu nome hacker é:M4STER"
+fi
+
+if [ "$nome" = "5" ]
+then
+printf $COR_AZUL_MARINHO
+echo "Seu nome hacker é:PRIV4T3"
+fi
+
+if [ "$nome" = "6" ]
+then
+printf $COR_AZUL_MARINHO
+echo "Seu nome hacker é:NÜLL"
+fi
+
+if [ "$nome" = "7" ]
+then
+printf $COR_AZUL_MARINHO
+echo "Seu nome hacker é:G3N3RÄL"
+fi
+
+if [ "$nome" = "8" ]
+then
+printf $COR_AZUL_MARINHO
+echo "Seu nome hacker é:1Č3"
+fi
+
+if [ "$nome" = "9" ]
+then
+printf $COR_AZUL_MARINHO
+echo "Seu nome hacker é:TØX1Č"
+fi
+
+if [ "$nome" = "10" ]
+then
+printf $COR_AZUL_MARINHO
+echo "Seu nome hacker é:C4Ä§H"
+fi
+
+if [ "$nome" = "11" ]
+then
+printf $COR_AZUL_MARINHO
+echo "Seu nome hacker é:§7EÅLPH"
+fi
+
+if [ "$nome" = "12" ]
+then
+printf $COR_AZUL_MARINHO
+echo "Seu nome hacker é:C4PT41N"
+fi
+
+if [ "$nome" = "13" ]
+then
+printf $COR_AZUL_MARINHO
+echo "Seu nome hacker é:PH4NTØM"
+fi
+
+if [ "$nome" = "14" ]
+then
+printf $COR_AZUL_MARINHO
+echo "Seu nome hacker é:BYT3"
+fi
+
+if [ "$nome" = "15" ]
+then
+printf $COR_AZUL_MARINHO
+echo "Seu nome hacker é:V1RÜ§"
+fi
+
+if [ "$nome" = "16" ]
+then
+printf $COR_AZUL_MARINHO
+echo "Seu nome hacker é:ØM1Č4ÖN"
+fi
+
+if [ "$nome" = "17" ]
+then
+printf $COR_AZUL_MARINHO
+echo "Seu nome hacker é:N3TWØ4K"
+fi
+
+if [ "$nome" = "18" ]
+then
+printf $COR_AZUL_MARINHO
+ echo "Seu nome hacker é:§K3L3TØN"
+fi
+
+if [ "$nome" = "19" ]
+then
+printf $COR_AZUL_MARINHO
+echo "Seu nome hacker é:FRE4K"
+fi
+
+if [ "$nome" = "20" ]
+then
+printf $COR_AZUL_MARINHO
+echo "Seu nome hacker é:B3T4"
+fi
+
+if [ "$nome" = "21" ]
+then
+printf $COR_AZUL_MARINHO
+echo "Seu nome hacker é:TRØØL"
+fi
+
+if [ "$nome" = "22" ]
+then
+printf $COR_AZUL_MARINHO
+echo "Seu nome hacker é:Ø.TH3"
+fi
+
+if [ "$nome" = "23" ]
+then
+printf $COR_AZUL_MARINHO
+echo "Seu nome hacker é:W1R3"
+fi
+
+if [ "$nome" = "24" ]
+then
+printf $COR_AZUL_MARINHO
+echo "Seu nome hacker é:HYDRÄ"
+fi
+
+if [ "$nome" = "25" ]
+then
+printf $COR_AZUL_MARINHO
+echo "Seu nome hacker é:FLÜK3"
+fi
+
